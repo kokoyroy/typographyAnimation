b7d6b429ae21982dfd7c83548a50f87d8a9518c9
kokoyroy <kokoyroy@gmail.com>
2021-08-21 14:36:14 +0300

    finished

---
+++ app.js
@@ -0,0 +1,5 @@
+const logo = document.querySelectorAll('#logo path');
+console.log(logo);
+logo.forEach(path => console.log(path.getTotalLength()));
+console.log('gamot o');
+//# sourceMappingURL=app.js.map
